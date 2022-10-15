30a10eb80e0c1693f0629bbc36da92ac50be4437
lukemansahid <lukemansahid2020@gmail.com>
2022-10-15 04:52:45 -0700

    writing script task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *  * main - print the string in the put function
+ *   *
+ *    * Description: using the main function
+ *     Return: always 0
+ *
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %i byte(s)\n", sizeof(c));
+	printf("Size of an int: %i byte(s)\n", sizeof(i));
+	printf("Size of a long int: %i byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(lli));
+	printf("Size of a float: %i byte(s)\n", sizeof(f));
+	return (0);
+}
