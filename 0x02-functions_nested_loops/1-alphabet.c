78d31a1a6a884fc3f690ec974497ea730681b0a5
lukemansahid <lukemansahid2020@gmail.com>
2022-10-18 08:45:36 -0700

    prints alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int lukeman;
+
+	for (lukeman = 'a'; lukeman <= 'z' lukeman++)
+	{
+		_putchar(lukeman);
+	}
+
+	_putchar('\n');
+}
