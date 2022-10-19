1962b8a75e1788120bf75f7e4720fa1984ec33dc
lukemansahid <lukemansahid2020@gmail.com>
2022-10-18 18:06:36 -0700

    correct daily time display

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,9 @@
 #include "main.h"
-
 /**
- *  * times_table - prints the 9 times table, starting with 0
+ *  jack_bauer - prints every minute of the day of Jack Bauer
+ *  starting from 00:00 to 23:59
  */
-void times_table(void)
+void jack_bauer(void)
 {
 	int i, j;
 
